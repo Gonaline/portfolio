1867750694b898a40e668f36ddd71ce3f289dc5e
Aline GONCE <100461966+Gonaline@users.noreply.github.com>
2022-06-26 17:18:14 +0200

    Revert "Start"

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { ItemController, Project1Controller } = require("./controllers");
+const { ItemController } = require("./controllers");
 
 const router = express.Router();
 
@@ -9,11 +9,5 @@ router.get("/items/:id", ItemController.read);
 router.put("/items/:id", ItemController.edit);
 router.post("/items", ItemController.add);
 router.delete("/items/:id", ItemController.delete);
-
-router.get("/project1", Project1Controller.browse);
-router.get("/project1/:id", Project1Controller.read);
-router.put("/project1/:id", Project1Controller.edit);
-router.post("/project1", Project1Controller.add);
-router.delete("/project1/:id", Project1Controller.delete);
 
 module.exports = router;
