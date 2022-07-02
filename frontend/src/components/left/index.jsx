94d3549b49eb4a63c36f2c9f0fd3e7910fed54d9
Gonaline <aline-gonce_student2022@wilder.school>
2022-07-02 09:42:06 +0200

    transmission of the page's props to the left component

--- frontend/src/components/left/index.jsx
+++ frontend/src/components/left/index.jsx
@@ -1,9 +1,19 @@
+import PropTypes from "prop-types";
 import SLeft from "./style";
 
-export default function Left() {
+export default function Left({ title, subTitle, description, textColor }) {
   return (
     <SLeft>
-      <p>LEFT</p>
+      <p>{title}</p>
+      <p>{subTitle}</p>
+      <p>{description}</p>
+      <p>{textColor}</p>
     </SLeft>
   );
 }
+Left.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+};
--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,17 +1,31 @@
-import { useContext } from "react";
-import ctxProvider from "@services/context/Ctx";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 import Header from "@components/header";
 import Footer from "@components/footer";
 import Left from "@components/left";
 import About from "@components/about";
 
 export default function PageAbout() {
-  const { test, page } = useContext(ctxProvider);
-  console.warn(test, page);
+  const [pageAbout, setPageAbout] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}${"/page/about"}`)
+      .then(({ data }) => {
+        setPageAbout(data);
+      });
+  }, []);
+
   return (
     <section className="body">
       <Header />
-      <Left />
+      <Left
+        title={pageAbout.title}
+        subtitle={pageAbout.subtitle}
+        description={pageAbout.description}
+        textColor={pageAbout.colorRight}
+      />
       <About />
       <Footer />
     </section>
